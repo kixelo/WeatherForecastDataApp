026e78bf0b1a7a51f0f91e2a1b259266cb3647ff
kixelo <igorolexik@gmail.com>
2024-02-10 18:58:55 +0100

    Plot fake data

--- backend.py
+++ backend.py
@@ -1,20 +1,18 @@
 import requests
+import streamlit as st
 
 API_KEY = "487d5e31a2d60fcb21bd53e4f55c31f6"
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data['list']
     num_values = 8 * forecast_days
     filtered_data = filtered_data[:num_values]
-    if kind == "Temperature":
-        filtered_data = [i["main"]["temp"] for i in filtered_data]
-        return filtered_data
-    if kind == "Sky":
-        filtered_data = [i['weather'][0]['main'] for i in filtered_data]
-        return filtered_data
+    return filtered_data
+
 
 if __name__=="__main__":
-    print(get_data(place="Kosice", forecast_days=1, kind="Temperature"))
+    get_data(place="Kosice", forecast_days=1)
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import plotly.express as px # dir(px)
 from backend import get_data
+from datetime import datetime
 
+# Add title, text input, slider, selectobox, and subheader
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -10,17 +12,31 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 
-st.subheader(f"{option} for the next {days} days in {place}")
+st.subheader(f"{option} for the next {days} days in {place.capitalize()}")
 
-#def get_data(days):
-#    dates = ["2024-02-09", "2024-02-10", "2024-02-11"]
-#    temperatures = [10, 11, 9]
-#    temperatures = [days * i for i in temperatures]
-#    return dates, temperatures
 
-d, t = get_data(place, days, option)
+# Get the temperature/sky data
+if place:
+    try:
+        filtered_data = get_data(place, days)
 
-#d, t = get_data(days)
+        if option == "Temperature":
+        # Create a temperature plot
+            temperatures = [(i["main"]["temp"]-273.15) for i in filtered_data]
+            dates = [i['dt_txt'] for i in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x" : "Date", "y" : "Temperature (C)"})
+            st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={"x" : "Date", "y" : "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Sky":
+            images = {"Clear" : "images/clear.png", "Clouds" : "images/cloud.png", "Rain" : "images/rain.png", "Snow" : "images/snow.png"}
+            sky_conditions = [i['weather'][0]['main'] for i in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions] # 'condition' just variable
+            dt = [i['dt'] for i in filtered_data]
+            dt_formatted = [datetime.utcfromtimestamp(i).strftime(('%a, %d-%m-%Y %H:%M')) for i in dt]
+            st.image(image_paths, width=115, caption=dt_formatted)
+
+    except KeyError:
+        st.subheader(f":red[Sorry we do not have '{place.capitalize()}' in our database!!!]")
+
+
+
